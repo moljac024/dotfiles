806d5b9c89fddb5dca6475e64d80ddd1a37892f2
Bojan Matić <matic.bojan@gmail.com>
2017-10-04 21:23:05 +0200

    Add common apps install to provision.sh

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -2,8 +2,10 @@
 echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 
 # Install build-deps for ruby
-apt-get install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
+sudo apt-get install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
 
 # Install build-deps for python
-apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils
 
+# Install common apps
+sudo apt-get install git git-gui gitk vim tmux

cf710ef67029f546f3ceae36eeaa42f6759ff181
Bojan Matić <matic.bojan@gmail.com>
2017-10-04 22:26:36 +0200

    Add htop and ncdu to provision

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -8,4 +8,4 @@ sudo apt-get install -y autoconf bison build-essential libssl-dev libyaml-dev li
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils
 
 # Install common apps
-sudo apt-get install git git-gui gitk vim tmux
+sudo apt-get install -y git git-gui gitk vim tmux htop ncdu

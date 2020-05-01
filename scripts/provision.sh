e9be9568c4cb6f8a36ad7c895a510065f65697ce
Bojan Matić <matic.bojan@gmail.com>
2020-05-01 11:16:19 +0200

    Remove useless crap from provision script too

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,22 +1,6 @@
-# Raise inotify limit - This is safe as these dotfiles are meant to be used on a personal ubuntu install. I wouldn't do this on a server that is publically available
+#!/bin/bash
+
+# Raise inotify limit - This is safe as these dotfiles are meant to be used on
+# a personal ubuntu install. I wouldn't do this on a server that is publically
+# available
 echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
-
-# Install common apps
-sudo apt-get install -y inotify-tools git git-gui gitk vim tmux htop ncdu
-
-sudo apt-get install -y gpg dirmngr
-
-# Install build deps for python
-sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
-libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
-xz-utils tk-dev libffi-dev liblzma-dev
-
-# Install build deps for ruby
-sudo apt-get install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm5 libgdbm-dev
-
-# Install build deps for erlang
-sudo apt-get install -y build-essential autoconf m4 \
-    libncurses5-dev \
-    libwxgtk3.0-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libpng-tools \
-    unixodbc-dev \
-    xsltproc fop

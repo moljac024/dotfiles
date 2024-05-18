1e30cebac4de7719d5b0ab284e8efa6ce46c1d91
Bojan Matić <matic.bojan@gmail.com>
2024-05-18 11:45:18 +0200

    Update nvim

--- src/nvim/lua/plugins/editing.lua
+++ src/nvim/lua/plugins/editing.lua
@@ -1,10 +1,49 @@
 return {
   {
-    "smoka7/hop.nvim",
-    version = "*",
-    opts = {
-      keys = "etovxqpdygfblzhckisuran",
-    },
+    "ggandor/leap.nvim",
+    config = function()
+      local leap = require("leap")
+      local notify = require("notify")
+
+      -- Disable auto-jumping to the first match
+      leap.opts.safe_labels = {}
+
+      leap.opts.equivalence_classes = {
+        "aá",
+        "eé",
+        "ií",
+        "sš",
+        "cć",
+        "cč",
+      }
+
+      local function activateKeymaps(variant)
+        if variant == "default" then
+          vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap-forward)")
+          vim.keymap.set({ "n", "x", "o" }, "S", "<Plug>(leap-backward)")
+          vim.keymap.set({ "n", "x", "o" }, "gs", "<Plug>(leap-from-window)")
+
+          return
+        end
+
+        if variant == "alternative" then
+          vim.keymap.set("n", "s", "<Plug>(leap)")
+          vim.keymap.set("n", "S", "<Plug>(leap-from-window)")
+          vim.keymap.set({ "x", "o" }, "s", "<Plug>(leap-forward)")
+          vim.keymap.set({ "x", "o" }, "S", "<Plug>(leap-backward)")
+
+          return
+        end
+
+        -- Show warning about unknown variant
+        if variant ~= nil then
+          notify("Unknown leap variant: " .. variant)
+        end
+      end
+
+      -- activateKeymaps()
+      activateKeymaps("default")
+    end,
   },
   {
     "mcauley-penney/tidy.nvim",
--- src/nvim/lua/plugins/ui.lua
+++ src/nvim/lua/plugins/ui.lua
@@ -1,5 +1,14 @@
 return {
   {
+    "rcarriga/nvim-notify",
+    config = function()
+      local notify = require("notify")
+
+      -- Make this the default notify fn
+      vim.notify = notify
+    end,
+  },
+  {
     "2kabhishek/nerdy.nvim",
     dependencies = {
       "stevearc/dressing.nvim",

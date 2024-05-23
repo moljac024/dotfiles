9bdc229742a8ade3b737c01c9110e512e164433b
Bojan Matić <matic.bojan@gmail.com>
2024-05-23 15:39:11 +0200

    [nvim] Move comment plugin

--- src/nvim/lua/plugins/editing.lua
+++ src/nvim/lua/plugins/editing.lua
@@ -44,6 +44,23 @@ return {
       })
     end,
   },
+  -- Sane commenting for JSX/TSX and other mixed language files
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require("ts_context_commentstring").setup({
+        enable_autocmd = false,
+      })
+
+      local get_option = vim.filetype.get_option
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.filetype.get_option = function(filetype, option)
+        return option == "commentstring" and require("ts_context_commentstring.internal").calculate_commentstring()
+          or get_option(filetype, option)
+      end
+    end,
+  },
   {
     "AckslD/nvim-neoclip.lua",
     dependencies = {

1ca70e947287aa28ae2c541bf32ea557669fd430
Bojan Matić <matic.bojan@gmail.com>
2024-05-24 09:37:37 +0200

    [nvim] Configure eslint

--- src/nvim/lua/plugins/editing.lua
+++ src/nvim/lua/plugins/editing.lua
@@ -188,6 +188,12 @@ return {
   {
     "mfussenegger/nvim-lint",
     config = function()
+      require("lint").linters_by_ft = {
+        javascript = { "eslint" },
+        typescript = { "eslint" },
+        javascriptreact = { "eslint" },
+        typescriptreact = { "eslint" },
+      }
       vim.api.nvim_create_autocmd({ "BufWritePost" }, {
         callback = function()
           -- try_lint without arguments runs the linters defined in `linters_by_ft`

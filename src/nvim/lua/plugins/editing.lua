1c1431e2a3330d0525de6b6659e9a4d40f3acba1
Bojan Matić <matic.bojan@gmail.com>
2024-05-26 14:14:19 +0200

    Add html/jsx pair plugin

--- src/nvim/lua/plugins/editing.lua
+++ src/nvim/lua/plugins/editing.lua
@@ -114,6 +114,27 @@ return {
     end,
   },
   {
+    "windwp/nvim-ts-autotag",
+    config = function()
+      require("nvim-ts-autotag").setup({
+        opts = {
+          -- Defaults
+          enable_close = true, -- Auto close tags
+          enable_rename = true, -- Auto rename pairs of tags
+          enable_close_on_slash = false, -- Auto close on trailing </
+        },
+        -- Also override individual filetype configs, these take priority.
+        -- Empty by default, useful if one of the "opts" global settings
+        -- doesn't work well in a specific filetype
+        per_filetype = {
+          -- ["html"] = {
+          --   enable_close = false,
+          -- },
+        },
+      })
+    end,
+  },
+  {
     "ggandor/leap.nvim",
     config = function()
       local leap = require("leap")

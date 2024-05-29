073587e4d9897635bacaeebbc80268151a978ff2
Bojan Matić <matic.bojan@gmail.com>
2024-05-29 18:08:37 +0200

    update nvim

--- src/nvim/lua/plugins/terminal.lua
+++ src/nvim/lua/plugins/terminal.lua
@@ -21,7 +21,7 @@ return {
 
       function _G.set_terminal_keymaps()
         local opts = { buffer = 0 }
-        -- vim.keymap.set("t", "<esc><esc><esc>", [[<C-\><C-n>]], opts)
+        vim.keymap.set("t", "<esc><esc><esc>", [[<C-\><C-n>]], opts)
         vim.keymap.set("t", "<A-h>", [[<Cmd>wincmd h<CR>]], opts)
         vim.keymap.set("t", "<A-j>", [[<Cmd>wincmd j<CR>]], opts)
         vim.keymap.set("t", "<A-k>", [[<Cmd>wincmd k<CR>]], opts)

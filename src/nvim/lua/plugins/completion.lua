89dc91dd390526d18c363457c393e43a2846c9b2
Bojan Matić <matic.bojan@gmail.com>
2024-06-02 23:03:26 +0200

    add some copilot <-> cmp integration

--- src/nvim/lua/plugins/completion.lua
+++ src/nvim/lua/plugins/completion.lua
@@ -110,6 +110,14 @@ return {
         },
       })
 
+      -- Disable copilot panel when nvim-cmp menu is opened
+      cmp.event:on("menu_opened", function()
+        vim.b.copilot_suggestion_hidden = true
+      end)
+      cmp.event:on("menu_closed", function()
+        vim.b.copilot_suggestion_hidden = false
+      end)
+
       luasnip.config.set_config({
         history = false,
         updateevents = "TextChanged,TextChangedI",

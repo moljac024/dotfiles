9a13ba8765d4a8b7b7a329b992baf68b5e4912c2
Bojan Matić <matic.bojan@gmail.com>
2024-06-04 19:58:15 +0200

    update

--- src/nvim/lua/plugins/ui.lua
+++ src/nvim/lua/plugins/ui.lua
@@ -245,6 +245,16 @@ return {
           end
         end,
       })
+
+      vim.api.nvim_create_autocmd("BufEnter", {
+        desc = "Pin the buffer to any window that is fixed width or height",
+        callback = function(args)
+          local stickybuf = require("stickybuf")
+          if not stickybuf.is_pinned() and (vim.wo.winfixwidth or vim.wo.winfixheight) then
+            stickybuf.pin()
+          end
+        end,
+      })
     end,
   },
 }

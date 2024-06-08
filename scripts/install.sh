810ccb8b2a11dbc1181a32609a8bd14d27908b1d
Bojan Matić <matic.bojan@gmail.com>
2024-06-08 15:16:17 +0200

    update dotfiles

--- scripts/install.sh
+++ scripts/install.sh
@@ -61,13 +61,7 @@ link src/kitty $HOME/.config/kitty
 link src/wezterm $HOME/.config/wezterm
 
 ################################################################################
-### Dircolors
-################################################################################
-
-link src/dircolors/solarized-ansi-light $HOME/.dir_colors
-
-################################################################################
-### Bash
+### Shells
 ################################################################################
 
 link src/bash/.bash.mine $HOME/.bash.mine
@@ -75,6 +69,8 @@ link src/bash/.bash_profile $HOME/.bash_profile
 link src/bash/.bashrc $HOME/.bashrc
 link src/bash/.inputrc $HOME/.inputrc
 link src/bash/.complete_alias $HOME/.bash_complete_alias
+
+link src/dircolors/solarized-ansi-light $HOME/.dir_colors
 
 ################################################################################
 ### Git
@@ -90,38 +86,27 @@ link src/git/.gitignore_global $HOME/.gitignore_global
 mkdir -p $HOME/.vim/autoload
 link src/vim/autoload/plug.vim $HOME/.vim/autoload/plug.vim
 link src/vim/vimrc $HOME/.vim/vimrc
-
-################################################################################
-### Neovim
-################################################################################
-
+# Neovim
 link src/nvim $HOME/.config/nvim
 
 ################################################################################
-### Ripgrep
+### Tools
 ################################################################################
 
 link src/ripgrep/.ripgreprc $HOME/.ripgreprc
-
-################################################################################
-### Tmux
-################################################################################
-
 link src/tmux/.tmux.conf $HOME/.tmux.conf
-link src/tmuxinator $HOME/.config/tmuxinator
+link src/k9s $HOME/.config/k9s
 
 ################################################################################
-### Asdf version manager
+### Version managers
 ################################################################################
 
+# Asdf
 if [ ! -d "$HOME/.asdf" ]; then
     git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
 fi
 
-################################################################################
-### Volta nodejs version manager
-################################################################################
-
+# Volta nodejs version manager
 if [ ! -d "$HOME/.volta" ]; then
     curl https://get.volta.sh | bash -s -- --skip-setup
 fi

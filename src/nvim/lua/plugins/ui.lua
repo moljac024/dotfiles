4b611f40a60a8570a87fcac60139517a27414116
Bojan Matić <matic.bojan@gmail.com>
2024-06-16 17:08:35 +0200

    update nvim config

--- src/nvim/lua/plugins/ui.lua
+++ src/nvim/lua/plugins/ui.lua
@@ -179,38 +179,6 @@ return {
           cmd = "<CMD>Telescope projects<CR>",
         },
       }, { set = true, show = true, cat = "telescope" })
-
-      -- Monkey patch keymap set for commander
-      local vim_keymap_set = vim.keymap.set
-      ---@diagnostic disable-next-line: duplicate-set-field
-      vim.keymap.set = function(...)
-        local args = { ... }
-        local modes = args[1]
-        local lhs = args[2]
-        local rhs = args[3]
-        local opts = args[4] or {}
-        local should_add = opts.desc and opts.desc ~= "" and type(opts.commander) == "table"
-
-        if should_add then
-          commander.add({
-            {
-              keys = {
-                { modes, lhs },
-              },
-              desc = opts.desc or "",
-              cmd = rhs,
-            },
-          }, {
-            set = false,
-            show = true,
-            cat = opts.commander.cat,
-          })
-        end
-
-        -- Remove extra keys from opts
-        opts.commander = nil
-        vim_keymap_set(...)
-      end
     end,
   },
   {
---
+++ src/nvim/lua/util/keybinds.lua
@@ -0,0 +1,42 @@
+local M = {}
+
+-- Monkey patch keymap set so that it supports adding commands to commander
+M.patch_keymap_set_for_commander = function()
+  local vim_keymap_set = vim.keymap.set
+
+  ---@diagnostic disable-next-line: duplicate-set-field
+  vim.keymap.set = function(...)
+    local commander_ok, commander = pcall(require, "commander")
+    local args = { ... }
+    local modes = args[1]
+    local lhs = args[2]
+    local rhs = args[3]
+    local opts = args[4] or {}
+    local should_add = commander_ok
+      and type(opts.desc) == "string"
+      and opts.desc ~= ""
+      and type(opts.commander) == "table"
+
+    if should_add then
+      commander.add({
+        {
+          keys = {
+            { modes, lhs },
+          },
+          desc = opts.desc or "",
+          cmd = rhs,
+        },
+      }, {
+        set = false,
+        show = true,
+        cat = opts.commander.cat,
+      })
+    end
+
+    -- Remove extra keys from opts
+    opts.commander = nil
+    vim_keymap_set(...)
+  end
+end
+
+return M

7b61b71b235a129cb97bba83f99d6035d3e6e38f
Bojan Matić <matic.bojan@gmail.com>
2024-08-25 09:12:29 +0200

    update

--- nvim/lua/plugins/editing.lua
+++ nvim/lua/plugins/editing.lua
@@ -246,11 +246,11 @@ return {
           lua = { "stylua" },
           -- Conform will run multiple formatters sequentially
           python = { "isort", "black" },
-          -- Use a sub-list to run only the first available formatter
-          javascript = { { "prettierd", "prettier" } },
-          typescript = { { "prettierd", "prettier" } },
-          javascriptreact = { { "prettierd", "prettier" } },
-          typescriptreact = { { "prettierd", "prettier" } },
+          -- Conform will run the first available formatter
+          javascript = { "prettierd", "prettier", stop_after_first = true },
+          typescript = { "prettierd", "prettier", stop_after_first = true },
+          javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+          typescriptreact = { "prettierd", "prettier", stop_after_first = true },
         },
         format_after_save = {
           -- These options will be passed to conform.format()

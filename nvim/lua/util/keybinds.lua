1a5d768f78165dd4b22c03eb3675e64c3f20cce0
Bojan Matić <matic.bojan@gmail.com>
2024-12-21 16:31:28 +0100

    update

--- nvim/lua/util/keybinds.lua
+++ nvim/lua/util/keybinds.lua
@@ -1,23 +1,26 @@
 local M = {}
 
--- Monkey patch keymap set so that it supports adding commands to commander
+-- Monkey patch keymap set so that it supports additional functionality and
+-- integration with plugins
 M.patch_keymap_set_for_commander = function()
   local vim_keymap_set = vim.keymap.set
 
   ---@diagnostic disable-next-line: duplicate-set-field
   vim.keymap.set = function(...)
-    local commander_ok, commander = pcall(require, "commander")
     local args = { ... }
     local modes = args[1]
     local lhs = args[2]
     local rhs = args[3]
     local opts = args[4] or {}
-    local should_add = commander_ok
-      and type(opts.desc) == "string"
-      and opts.desc ~= ""
-      and type(opts.commander) == "table"
 
-    if should_add then
+    -- Commander integration
+    local commander_ok, commander = pcall(require, "commander")
+    local should_add_to_commander = commander_ok
+        and type(opts.desc) == "string"
+        and opts.desc ~= ""
+        and type(opts.commander) == "table"
+
+    if should_add_to_commander then
       commander.add({
         {
           keys = {
@@ -32,9 +35,9 @@ M.patch_keymap_set_for_commander = function()
         cat = opts.commander.cat,
       })
     end
-
     -- Remove extra keys from opts
     opts.commander = nil
+
     vim_keymap_set(...)
   end
 end

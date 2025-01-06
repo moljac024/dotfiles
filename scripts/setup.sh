e5936f9b226732c576761e2ad1dbd7ca92c5c344
Bojan Matić <matic.bojan@gmail.com>
2025-01-06 10:41:31 +0100

    update

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -16,14 +16,6 @@ if [[ ! -e $HOME/.config ]] ; then
 fi
 
 ################################################################################
-
-################################################################################
-### Starship shell prompt
-################################################################################
-
-ensure_symlink $ROOT_DIR/starship/starship.toml $HOME/.config/starship.toml
-
-################################################################################
 ### Scripts
 ################################################################################
 
@@ -37,10 +29,16 @@ fi
 ### Terminals
 ################################################################################
 
+# Foot
 ensure_symlink $ROOT_DIR/foot $HOME/.config/foot
+
+# Kitty
 ensure_symlink $ROOT_DIR/kitty $HOME/.config/kitty
+
+# Wezterm
 ensure_symlink $ROOT_DIR/wezterm $HOME/.config/wezterm
 
+# Konsole
 mkdir -p $HOME/.local/share
 ensure_symlink $ROOT_DIR/konsole/konsolerc $HOME/.config/konsolerc
 ensure_symlink $ROOT_DIR/konsole/local $HOME/.local/share/konsole
@@ -48,6 +46,9 @@ ensure_symlink $ROOT_DIR/konsole/local $HOME/.local/share/konsole
 ################################################################################
 ### Shells
 ################################################################################
+
+# Starship shell prompt
+ensure_symlink $ROOT_DIR/starship/starship.toml $HOME/.config/starship.toml
 
 # Common shell scripts, utilities and config
 ensure_symlink $ROOT_DIR/shell $HOME/.shell
@@ -69,27 +70,18 @@ ensure_symlink $ROOT_DIR/fish $HOME/.config/fish
 ensure_symlink $ROOT_DIR/shell/.inputrc $HOME/.inputrc
 
 ################################################################################
-### Git
+### Dev
 ################################################################################
 
+# Git
 ensure_symlink $ROOT_DIR/git/.gitconfig $HOME/.gitconfig
 ensure_symlink $ROOT_DIR/git/.gitignore_global $HOME/.gitignore_global
 
-################################################################################
-### SSH
-################################################################################
-
+# SSH
 ensure_symlink $ROOT_DIR/ssh $HOME/.ssh
 
-################################################################################
-### K8S
-################################################################################
-
+# K8s
 ensure_symlink $ROOT_DIR/kube $HOME/.kube
-
-################################################################################
-### Text editors
-################################################################################
 
 # Vim
 mkdir -p $HOME/.vim/autoload
@@ -102,16 +94,33 @@ ensure_symlink $ROOT_DIR/nvim $HOME/.config/nvim
 # Helix
 ensure_symlink $ROOT_DIR/helix $HOME/.config/helix
 
+# Volta nodejs version manager
+if [ ! -d "$HOME/.volta" ]; then
+    curl https://get.volta.sh | bash -s -- --skip-setup
+fi
+
+# Mise dev tool manager
+if ! is_command mise; then
+  curl https://mise.run | sh
+fi
+ensure_symlink $ROOT_DIR/mise $HOME/.config/mise
+
 ################################################################################
-### Tools
+### CLI Tools
 ################################################################################
 
+# Ripgrep
 ensure_symlink $ROOT_DIR/ripgrep/.ripgreprc $HOME/.ripgreprc
 
+# Tmux
 mkdir -p $HOME/.tmux/plugins
 git_clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 ensure_symlink $ROOT_DIR/tmux/.tmux.conf $HOME/.tmux.conf
 
+# Zellij
+ensure_symlink $ROOT_DIR/zellij $HOME/.config/zellij
+
+# K9s
 ensure_symlink $ROOT_DIR/k9s $HOME/.config/k9s
 
 # Midnight commander
@@ -120,52 +129,13 @@ ensure_symlink $ROOT_DIR/mc $HOME/.config/mc
 ensure_symlink $ROOT_DIR/mc/skins $HOME/.local/share/mc/skins
 
 ################################################################################
-### Other apps
+### Desktop apps
 ################################################################################
 
 # Mpv
 ensure_symlink $ROOT_DIR/mpv $HOME/.config/mpv
 
-################################################################################
-### Version managers
-################################################################################
-
-# Volta nodejs version manager
-if [ ! -d "$HOME/.volta" ]; then
-    curl https://get.volta.sh | bash -s -- --skip-setup
-fi
-
-if ! is_command mise; then
-  # Mise dev tool manager
-  curl https://mise.run | sh
-fi
-ensure_symlink $ROOT_DIR/mise $HOME/.config/mise
-
-################################################################################
-### DEs and WMs
-################################################################################
-
-ensure_symlink $ROOT_DIR/wofi $HOME/.config/wofi
-ensure_symlink $ROOT_DIR/rofi $HOME/.config/rofi
-ensure_symlink $ROOT_DIR/fuzzel $HOME/.config/fuzzel
-mkdir -p $HOME/.local/share/rofi
-ensure_symlink $ROOT_DIR/rofi/themes $HOME/.local/share/rofi/themes
-
-ensure_symlink $ROOT_DIR/mako $HOME/.config/mako
-ensure_symlink $ROOT_DIR/dunst $HOME/.config/dunst
-ensure_symlink $ROOT_DIR/wlogout $HOME/.config/wlogout
-
-ensure_symlink $ROOT_DIR/waybar $HOME/.config/waybar
-
-ensure_symlink $ROOT_DIR/swaylock $HOME/.config/swaylock
-ensure_symlink $ROOT_DIR/sway $HOME/.config/sway
-
-ensure_symlink $ROOT_DIR/hypr $HOME/.config/hypr
-
-################################################################################
-### Kando
-################################################################################
-
+# Kando
 mkdir -p $HOME/.config/kando
 ensure_symlink $ROOT_DIR/kando/config.json $HOME/.config/kando/config.json
 ensure_symlink $ROOT_DIR/kando/menus.json $HOME/.config/kando/menus.json
@@ -175,14 +145,50 @@ ensure_symlink $ROOT_DIR/kando/config.json $HOME/.var/app/menu.kando.Kando/confi
 ensure_symlink $ROOT_DIR/kando/menus.json $HOME/.var/app/menu.kando.Kando/config/kando/menus.json
 
 ################################################################################
+### DEs and WMs
+################################################################################
+
+# Fuzzel
+ensure_symlink $ROOT_DIR/fuzzel $HOME/.config/fuzzel
+
+# Rofi
+mkdir -p $HOME/.local/share/rofi
+ensure_symlink $ROOT_DIR/rofi/themes $HOME/.local/share/rofi/themes
+ensure_symlink $ROOT_DIR/rofi $HOME/.config/rofi
+
+# Wofi
+ensure_symlink $ROOT_DIR/wofi $HOME/.config/wofi
+
+# Mako
+ensure_symlink $ROOT_DIR/mako $HOME/.config/mako
+
+# Dunst
+ensure_symlink $ROOT_DIR/dunst $HOME/.config/dunst
+
+# Wlogout
+ensure_symlink $ROOT_DIR/wlogout $HOME/.config/wlogout
+
+# Waybar
+ensure_symlink $ROOT_DIR/waybar $HOME/.config/waybar
+
+# Sway
+ensure_symlink $ROOT_DIR/swaylock $HOME/.config/swaylock
+ensure_symlink $ROOT_DIR/sway $HOME/.config/sway
+
+# Hyprland
+ensure_symlink $ROOT_DIR/hypr $HOME/.config/hypr
+
+################################################################################
 ### Applications (.desktop files)
 ################################################################################
+
 mkdir -p $HOME/.local/share/applications &&\
 cp -r $ROOT_DIR/applications/*.desktop $HOME/.local/share/applications
 
 ################################################################################
 ### Other
 ################################################################################
+
 mkdir -p $HOME/.local/share/nautilus/
 ensure_symlink $ROOT_DIR/nautilus/scripts $HOME/.local/share/nautilus/scripts
 ensure_symlink $ROOT_DIR/nautilus/actions $HOME/.local/share/nautilus/actions

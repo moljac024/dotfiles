bf118b8a8581ebd787528555f18e608076a803b9
Bojan Matić <matic.bojan@gmail.com>
2025-02-12 12:00:22 +0100

    [nvim] [minor] remove comment

--- nvim/lua/plugins/util.lua
+++ nvim/lua/plugins/util.lua
@@ -5,9 +5,6 @@ return {
     priority = 1000,
     lazy = false,
     opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
       rename = { enabled = true },
     }
   },

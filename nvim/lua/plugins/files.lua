ea09b1572e41d908b59a1b0e6ddbb043be8c645a
Bojan Matić <matic.bojan@gmail.com>
2025-02-19 15:55:11 +0100

    remove telescope integration from harpoon and hbac

--- nvim/lua/plugins/files.lua
+++ nvim/lua/plugins/files.lua
@@ -94,26 +94,6 @@ return {
         },
       })
 
-      -- basic telescope configuration
-      local conf = require("telescope.config").values
-      local function toggle_telescope(harpoon_files)
-        local file_paths = {}
-        for _, item in ipairs(harpoon_files.items) do
-          table.insert(file_paths, item.value)
-        end
-
-        require("telescope.pickers")
-            .new({}, {
-              prompt_title = "Harpoon",
-              finder = require("telescope.finders").new_table({
-                results = file_paths,
-              }),
-              previewer = conf.file_previewer({}),
-              sorter = conf.generic_sorter({}),
-            })
-            :find()
-      end
-
       vim.keymap.set("n", "<C-e>", function()
         harpoon.ui:toggle_quick_menu(harpoon:list())
       end, { desc = "Open harpoon quick menu" })
@@ -125,12 +105,6 @@ return {
             harpoon:list():add()
           end
         },
-        {
-          desc = "Open harpoon window",
-          cmd = function()
-            toggle_telescope(harpoon:list())
-          end
-        }
       }, { cat = "harpoon" })
     end,
   },
--- nvim/lua/plugins/util.lua
+++ nvim/lua/plugins/util.lua
@@ -21,12 +21,7 @@ return {
           vim.api.nvim_buf_delete(bufnr, {})
         end,
         close_buffers_with_windows = false, -- hbac will close buffers with associated windows if this option is `true`
-        telescope = {},
       })
-
-      pcall(require("telescope").load_extension, "hbac")
-
-      vim.keymap.set("n", "<leader>b", "<CMD>Telescope hbac buffers<CR>", { desc = "Search buffers", commander = {} })
     end,
   },
 }

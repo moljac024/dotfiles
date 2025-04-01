5cc77e62384d46b78a00342eabedee310f36b9f0
Bojan Matić <matic.bojan@gmail.com>
2025-04-01 20:55:24 +0200

    [nvim] add back nvim-tree

--- nvim/lua/plugins/files.lua
+++ nvim/lua/plugins/files.lua
@@ -52,6 +52,70 @@ return {
     end,
   },
   {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    config = function()
+      local function on_attach(bufnr)
+        local api = require "nvim-tree.api"
+
+        local function opts(desc)
+          return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+
+        local function edit_or_open()
+          local node = api.tree.get_node_under_cursor()
+
+          if node.nodes ~= nil then
+            -- expand or collapse folder
+            api.node.open.edit()
+          else
+            -- open file
+            api.node.open.edit()
+            -- Close the tree if file was opened
+            api.tree.close()
+          end
+        end
+
+        -- open as vsplit on current node
+        local function vsplit_preview()
+          local node = api.tree.get_node_under_cursor()
+
+          if node.nodes ~= nil then
+            -- expand or collapse folder
+            api.node.open.edit()
+          else
+            -- open file as vsplit
+            api.node.open.vertical()
+          end
+
+          -- Finally refocus on tree if it was lost
+          api.tree.focus()
+        end
+
+        -- default mappings
+        api.config.mappings.default_on_attach(bufnr)
+
+        vim.keymap.set("n", "l", edit_or_open, opts("Edit Or Open"))
+        vim.keymap.set("n", "L", vsplit_preview, opts("Vsplit Preview"))
+        vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close parent"))
+        vim.keymap.set("n", "H", api.tree.collapse_all, opts("Collapse All"))
+      end
+
+
+      require("nvim-tree").setup({
+        on_attach = on_attach
+      })
+
+      vim.keymap.set(
+        "n",
+        "<C-h>",
+        ":NvimTreeToggle<cr>",
+        { silent = true, noremap = true }
+      )
+    end,
+  },
+  {
     -- Project file navigation
     "ThePrimeagen/harpoon",
     branch = "harpoon2",

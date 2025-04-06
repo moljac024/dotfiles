2226edd605b6d852f3b63cf9f5d2b10e9831b553
Bojan Matić <matic.bojan@gmail.com>
2025-04-06 10:40:36 +0200

    update neovim deps; add outline plugin again

--- nvim/lua/plugins/editing.lua
+++ nvim/lua/plugins/editing.lua
@@ -44,6 +44,18 @@ return {
         end,
       })
     end,
+    {
+      "hedyhli/outline.nvim",
+      config = function()
+        -- Example mapping to toggle outline
+        vim.keymap.set("n", { "<leader>o", "<A-o>" }, "<cmd>Outline<CR>",
+          { desc = "Toggle Outline" })
+
+        require("outline").setup {
+          -- Your setup opts here (leave empty to use defaults)
+        }
+      end,
+    },
   },
   {
     "kylechui/nvim-surround",

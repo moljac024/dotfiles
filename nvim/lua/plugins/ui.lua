dc78905bc2d0bbe8cf74d478d85599fe3bc312ef
Bojan Matić <matic.bojan@gmail.com>
2025-06-23 04:21:47 +0200

    [nvim] configure tabby

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -184,6 +184,18 @@ return {
   {
     'nanozuki/tabby.nvim',
     config = function()
+      -- Show tabline always
+      vim.o.showtabline = 2
+
+      local theme = {
+        fill = 'TabLineFill',
+        head = 'TabLine',
+        current_tab = 'TabLineSel',
+        tab = 'TabLineSel',
+        win = 'TabLineSel',
+        tail = 'TabLineSel',
+      }
+
       require('tabby').setup({
         preset = "active_wins_at_tail",
         lualine_theme = lualine_theme,
@@ -195,6 +207,45 @@ return {
         buf_name = {
           mode = 'unique', -- or 'relative', 'tail', 'shorten'
         },
+        line = function(line)
+          return {
+            {
+              { '', hl = theme.head },
+              line.sep('', theme.head, theme.fill),
+            },
+            line.tabs().foreach(function(tab)
+              local hl = tab.is_current() and theme.current_tab or theme.tab
+              return {
+                line.sep('', hl, theme.fill),
+                tab.is_current() and '' or '',
+                ' ',
+                tab.number(),
+                tab.name(),
+                tab.close_btn(''),
+                line.sep(' ', hl, theme.fill),
+                hl = hl,
+                margin = ' ',
+              }
+            end),
+            line.spacer(),
+            line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
+              return {
+                line.sep(' ', theme.win, theme.fill),
+                win.is_current() and '' or '',
+                ' ',
+                win.buf_name(),
+                line.sep(' ', theme.win, theme.fill),
+                hl = theme.win,
+                margin = ' ',
+              }
+            end),
+            {
+              line.sep(' ', theme.tail, theme.fill),
+              { '  ', hl = theme.tail },
+            },
+            hl = theme.fill,
+          }
+        end,
       })
     end,
   },

343f131ba36199b4a2c1e4b79da652b20384fb3c
Bojan Matić <matic.bojan@gmail.com>
2025-07-23 18:30:02 +0200

    refactor nvim init logic

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,7 +12,7 @@ require("lib.command").patch_keymap_set_for_commander()
 -- Config
 -- ############################################################################
 
-require("config")
+require("config.main")
 
 -- ############################################################################
 -- Lazy (plugin manager)
@@ -23,5 +23,8 @@ require("lib.lazy").init()
 -- ############################################################################
 -- Keybindings
 -- ############################################################################
+
+-- We require keybinds last so that we can reference plugins and to make sure
+-- plugins don't override any of our custom keybinds.
 
 require("config.keybinds")
--- nvim/lua/lib/lazy.lua
+++ nvim/lua/lib/lazy.lua
@@ -1,36 +1,9 @@
 local M = {}
-
-local function ensure_installed(plugin, branch)
-  ---@diagnostic disable-next-line: unused-local
-  local user, repo = string.match(plugin, "(.+)/(.+)")
-  local repo_path = vim.fn.stdpath("data") .. "/lazy/" .. repo
-  if not (vim.uv or vim.loop).fs_stat(repo_path) then
-    vim.notify("Installing " .. plugin .. " " .. branch)
-    local repo_url = "https://github.com/" .. plugin .. ".git"
-    local out = vim.fn.system({
-      "git",
-      "clone",
-      "--filter=blob:none",
-      "--branch=" .. branch,
-      repo_url,
-      repo_path
-    })
-    if vim.v.shell_error ~= 0 then
-      vim.api.nvim_echo({
-        { "Failed to clone " .. plugin .. ":\n", "ErrorMsg" },
-        { out,                                   "WarningMsg" },
-        { "\nPress any key to exit..." },
-      }, true, {})
-      vim.fn.getchar()
-      os.exit(1)
-    end
-  end
-  return repo_path
-end
+local git = import("./git")
 
 M.init = function()
-  local lazy_path = ensure_installed("folke/lazy.nvim", "stable")
-  local hotpot_path = ensure_installed("rktjmp/hotpot.nvim", "v0.14.6")
+  local lazy_path = git.ensure_installed("folke/lazy.nvim", { ref = "stable", refresh = false })       -- Package manager
+  local hotpot_path = git.ensure_installed("rktjmp/hotpot.nvim", { ref = "v0.14.8", refresh = false }) -- Fennel compiler
 
   -- As per Lazy's install instructions, but also include hotpot
   vim.opt.runtimepath:prepend({ hotpot_path, lazy_path })

0e2fab7940e4e5aa60afe7922b400d5786cb1ba4
Bojan Matić <matic.bojan@gmail.com>
2025-08-20 11:13:02 +0200

    update

--- shell/common.sh
+++ shell/common.sh
@@ -158,7 +158,7 @@ alias back='cd "$OLDPWD"'
 alias mkdir='mkdir -p -v'
 alias su='sudo -i'
 alias duf='du -sk * | sort -n | perl -ne '\''($s,$f)=split(m{\t});for (qw(K M G)) {if($s<1024) {printf("%.1f",$s);print "$_\t$f"; last};$s=$s/1024}'\'
-alias gs='git add . && git commit -m "Sync" && git push origin'
+alias gs='git add . && git commit -m "sync" && git push origin'
 alias erlang-version="erl -eval '{ok, Version} = file:read_file(filename:join([code:root_dir(), \"releases\", erlang:system_info(otp_release), \"OTP_VERSION\"])), io:fwrite(Version), halt().' -noshell"
 alias serve-spa="npx --yes http-server-spa"
 alias emacs="flatpak run org.gnu.emacs"

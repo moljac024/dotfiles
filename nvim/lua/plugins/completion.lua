2f186071f8047e9ca1f466082ea0617a8b055a99
Bojan Matić <matic.bojan@gmail.com>
2025-09-03 12:38:50 +0200

    update nvim config

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -110,13 +110,17 @@ return {
         )
       })
 
-      -- Disable copilot panel when nvim-cmp menu is opened
-      cmp.event:on("menu_opened", function()
-        vim.b.copilot_suggestion_hidden = true
-      end)
-      cmp.event:on("menu_closed", function()
-        vim.b.copilot_suggestion_hidden = false
-      end)
+      local function activate_copilot_hide()
+        -- Disable copilot panel when nvim-cmp menu is opened
+        cmp.event:on("menu_opened", function()
+          vim.b.copilot_suggestion_hidden = true
+        end)
+        cmp.event:on("menu_closed", function()
+          vim.b.copilot_suggestion_hidden = false
+        end)
+      end
+
+      activate_copilot_hide()
     end,
   },
 }

44255e6b4ff0e18a67661fda8a0ed4e8cf66eea7
Bojan Matić <matic.bojan@gmail.com>
2025-09-05 15:18:02 +0200

    add nvim-origami

--- nvim/lua/plugins/editing.lua
+++ nvim/lua/plugins/editing.lua
@@ -59,6 +59,17 @@ return {
     end,
   },
   {
+    "chrisgrieser/nvim-origami",
+    event = "VeryLazy",
+    opts = {}, -- needed even when using default config
+
+    -- recommended: disable vim's auto-folding
+    init = function()
+      vim.opt.foldlevel = 99
+      vim.opt.foldlevelstart = 99
+    end,
+  },
+  {
     "kylechui/nvim-surround",
     version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",

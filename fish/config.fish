41a455701c2078a77adc43de23c292643692adef
Bojan Matić <matic.bojan@gmail.com>
2026-02-14 01:02:45 +0100

    disable kubectl completions in fish
    dang are they sloooow

--- fish/config.fish
+++ fish/config.fish
@@ -7,9 +7,13 @@ source $DOTFILES/shell/fish/lib
 source $DOTFILES/shell/fish/common
 source $DOTFILES/shell/fish/prompt
 
-if not test -f "$DOTFILES/shell/fish/local/kubectl-completions"
-  kubectl completion fish > "$DOTFILES/shell/fish/local/kubectl-completions"
-end
+# NOTE: This tends to slow down shell startup on systems where kubectl is slow
+# to run. Also fish comes with kubectl completions already so this is not
+# needed.
+
+# if not test -f "$DOTFILES/shell/fish/local/kubectl-completions"
+#   kubectl completion fish > "$DOTFILES/shell/fish/local/kubectl-completions"
+# end
 
 # Local shell overrides, this should be last
 source_dir "$DOTFILES/shell/fish/local"

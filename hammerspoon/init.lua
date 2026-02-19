1e37ce5a2efb912d1da0d07320f23e06cc3ed2de
Bojan Matić <matic.bojan@gmail.com>
2026-02-19 15:21:04 +0100

    update hammerspoon config

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,71 +1,17 @@
 -- =============================================================================
--- == Auto reload
+-- == Setup
 -- =============================================================================
 
-hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", function(files)
-  local should_reload = false
-  for _, file in pairs(files) do
-    if file:sub(-4) == ".lua" then
-      should_reload = true
-    end
-  end
-  if should_reload then
-    hs.reload()
-  end
-end):start()
+local lib = require "lib"
 
--- =============================================================================
--- == Helper functions
--- =============================================================================
-
-local function safe_bind_key(mods, key, fn)
-  local mod_str = table.concat(mods, "+")
-  local action = "Assigning"
-
-  if not hs.hotkey.assignable(mods, key) then
-    hs.printf("Skipping bind of %s+%s (not assignable)", mod_str, key)
-    return nil
-  end
-
-  if type(fn) ~= "function" then
-    action = "Disabling"
-    fn = function() end
-  end
-
-  hs.printf("%s %s+%s", action, mod_str, key)
-  return hs.hotkey.bind(mods, key, fn)
-end
-
-local function lock_screen()
-  -- This relies on the existence of an app called Lock Screen. Can be created via Automator.
-  -- hs.application.launchOrFocus("Lock Screen.app") -- If you have the Lock Screen.app, can also be made via Automator
-
-  -- This function uses private Apple APIs and could therefore stop working in any given release of macOS without warning.
-  hs.caffeinate.lockScreen()
-end
-
--- GNOME mode: cmd+1..0 always "run or raise" apps in your chosen order.
-local function gnomify(dock_app_list)
-  if type(dock_app_list) ~= "table" then
-    hs.printf("warning: gnomify parameter is not a table")
-    return
-  end
-
-  for i = 1, 10 do
-    local key = (i == 10) and "0" or tostring(i)
-    safe_bind_key({ "cmd" }, key, function()
-      if type(dock_app_list[i]) == "string" then
-        hs.application.launchOrFocus(dock_app_list[i])
-      end
-    end)
-  end
-end
+lib.setupAutoReload()
+-- hs.window.animationDuration = 0
 
 -- =============================================================================
 -- == Main config
 -- =============================================================================
 
-gnomify({
+lib.gnomify({
   "Finder", -- Has to be in first slot
   "Ghostty.app",
   "Microsoft Edge.app",
@@ -85,9 +31,23 @@ gnomify({
   "Excalidraw.app"
 })
 
+-- Window management
+lib.bindKey({ "cmd", "alt" }, "h", function() lib.moveWindow(hs.window.focusedWindow(), "screen-left") end)
+lib.bindKey({ "cmd", "alt" }, "j", function() lib.moveWindow(hs.window.focusedWindow(), "screen-down") end)
+lib.bindKey({ "cmd", "alt" }, "k", function() lib.moveWindow(hs.window.focusedWindow(), "screen-up") end)
+lib.bindKey({ "cmd", "alt" }, "l", function() lib.moveWindow(hs.window.focusedWindow(), "screen-right") end)
+
+lib.bindKey({ "cmd", "alt" }, "up", function()
+  lib.handleWinWithoutAXEUI(hs.window.focusedWindow(), function(win)
+    win:maximize()
+  end)
+end)
+
 -- Kill app hiding keybinds
-safe_bind_key({ "cmd" }, "h", nil)
-safe_bind_key({ "cmd", "alt" }, "h", nil)
+lib.bindKey({ "cmd" }, "h", nil)
+lib.bindKey({ "cmd", "alt" }, "h", nil)
 
 -- Lock screen keybind
-safe_bind_key({ "cmd", "alt" }, "l", lock_screen)
+lib.bindKey({ "cmd", "ctrl", "alt" }, "l", lib.lockScreen)
+
+lib.bindKey({ "cmd" }, "'", lib.applicationChooser.invoke)
---
+++ hammerspoon/lib.lua
@@ -0,0 +1,142 @@
+local M = {}
+
+M.setupAutoReload = function()
+  hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", function(files)
+    local shouldReload = false
+    for _, file in pairs(files) do
+      if file:sub(-4) == ".lua" then
+        shouldReload = true
+      end
+    end
+    if shouldReload then
+      hs.reload()
+    end
+  end):start()
+end
+
+M.bindKey = function(mods, key, fn)
+  local modStr = table.concat(mods, "+")
+  local action = "Assigning"
+
+  if not hs.hotkey.assignable(mods, key) then
+    hs.printf("Skipping bind of %s+%s (not assignable)", modStr, key)
+    return nil
+  end
+
+  if type(fn) ~= "function" then
+    action = "Disabling"
+    fn = function() end
+  end
+
+  hs.printf("%s %s+%s", action, modStr, key)
+  return hs.hotkey.bind(mods, key, fn)
+end
+
+-- GNOME mode: cmd+1..0 always "run or raise" apps in your chosen order.
+M.gnomify = function(apps)
+  if type(apps) ~= "table" then
+    hs.printf("warning: gnomify parameter is not a table")
+    return
+  end
+
+  for i = 1, 10 do
+    local key = (i == 10) and "0" or tostring(i)
+    M.bindKey({ "cmd" }, key, function()
+      if type(apps[i]) == "string" then
+        hs.application.launchOrFocus(apps[i])
+      end
+    end)
+  end
+end
+
+M.lockScreen = function()
+  -- This relies on the existence of an app called Lock Screen. Can be created via Automator.
+  -- hs.application.launchOrFocus("Lock Screen.app") -- If you have the Lock Screen.app, can also be made via Automator
+
+  -- This function uses private Apple APIs and could therefore stop working in any given release of macOS without warning.
+  hs.caffeinate.lockScreen()
+end
+
+M.handleWinWithoutAXEUI = function(win, fn)
+  local axApp = hs.axuielement.applicationElement(win:application())
+  local enhanced = axApp.AXEnhancedUserInterface
+  if enhanced then
+    axApp.AXEnhancedUserInterface = false
+  end
+
+  fn(win)
+
+  hs.timer.doAfter(hs.window.animationDuration * 2, function()
+    axApp.AXEnhancedUserInterface = enhanced
+  end)
+end
+
+M.isWindowMaximized = function(win)
+  local frame = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  return max.x == frame.x and max.y == frame.y and max.w == frame.w and max.h == frame.h
+end
+
+M.moveWindow = function(win, direction)
+  M.handleWinWithoutAXEUI(win, function()
+    local maximized = M.isWindowMaximized(win)
+    if direction == "screen-left" then
+      win:moveOneScreenEast()
+    end
+    if direction == "screen-down" then
+      win:moveOneScreenSouth()
+    end
+    if direction == "screen-right" then
+      win:moveOneScreenWest()
+    end
+    if direction == "screen-up" then
+      win:moveOneScreenNorth()
+    end
+
+    if maximized then
+      win:maximize()
+    end
+  end)
+end
+
+local function makeApplicationSwitchChooser()
+  local chooser = hs.chooser.new(function(x)
+    if x ~= nil then
+      -- Focus app
+      hs.application.launchOrFocus(x.path)
+    end
+  end)
+
+  -- Clear query when closed
+
+  chooser:hideCallback(function()
+    chooser:query(nil)
+  end)
+
+  local function invoke()
+    local apps = hs.fnutils.filter(hs.application.runningApplications(), function(app)
+      return not app:isHidden() and app:mainWindow() ~= nil and #app:visibleWindows() > 0
+    end)
+
+    local appChoices = hs.fnutils.map(apps, function(x)
+      return {
+        text = x:name(),
+        image = hs.image.iconForFile(x:path()),
+        path = x:path(),
+      }
+    end)
+
+    chooser:choices(appChoices)
+    chooser:show()
+  end
+
+  return {
+    invoke = invoke
+  }
+end
+
+M.applicationChooser = makeApplicationSwitchChooser()
+
+return M

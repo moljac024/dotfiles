bb8e3ee1fdefe6f9bb58a9ce0a5285922b8eccfc
Bojan Matić <matic.bojan@gmail.com>
2026-03-25 12:13:16 +0100

    [nvim] remove dashboard

--- nvim/lua/plugins/lazy/ui.lua
+++ nvim/lua/plugins/lazy/ui.lua
@@ -10,34 +10,6 @@ return {
       rename = {},
       input = {},
       git = {},
-      dashboard = {
-        sections = {
-          { section = "header" },
-          { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
-          { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-          {
-            icon = " ",
-            title = "Git Status",
-            section = "terminal",
-            enabled = function()
-              return Snacks.git.get_root() ~= nil
-            end,
-            cmd = "git status --short --branch --renames",
-            height = 5,
-            padding = 1,
-            ttl = 5 * 60,
-            indent = 3,
-          },
-          { section = "startup" },
-        },
-        preset = {
-          pick = nil,
-          keys = {
-            { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", },
-            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-          },
-        },
-      },
       image = {},
     }
   },

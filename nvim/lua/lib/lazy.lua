602f492f07326c2e0f33c08538063e0f35e0c63b
Bojan Matić <matic.bojan@gmail.com>
2026-03-30 10:38:13 +0200

    remove hotpot from lazy.lua

--- nvim/lua/lib/lazy.lua
+++ nvim/lua/lib/lazy.lua
@@ -1,22 +1,14 @@
 local M = {}
 local git = require("lib.git")
 
-M.init = function()
-  local lazy_path = git.ensure_installed("folke/lazy.nvim", { ref = "stable", refresh = false })       -- Package manager
-  local hotpot_path = git.ensure_installed("rktjmp/hotpot.nvim", { ref = "v0.14.8", refresh = false }) -- Fennel compiler
-
-  -- As per Lazy's install instructions, but also include hotpot
-  vim.opt.runtimepath:prepend({ hotpot_path, lazy_path })
-  -- You must call vim.loader.enable() before requiring hotpot unless you are
-  -- passing {performance = {cache = false}} to Lazy.
-  vim.loader.enable()
-  require("hotpot")
+M.init = function(plugins_dir)
+  git.ensure_installed("folke/lazy.nvim", { ref = "stable", refresh = false }) -- Package manager
 
   -- Load list of plugins
-  local plugins = require("plugins.lazy")
+  local plugins = require(plugins_dir)
 
   require("lazy").setup({
-    spec = vim.list_extend({ "rktjmp/hotpot.nvim" }, plugins),
+    spec = plugins,
     change_detection = {
       notify = false,
     },
